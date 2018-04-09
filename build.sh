d1f37dbd8f0e8a4e1ce9c93cc54ecbff4de3518a
shiyicode <458241107@qq.com>
2018-04-09 09:44:24 +0800

    iniat

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+workspace=$(cd $(dirname $0) && pwd -P)
+cd $workspace
+
+#const
+app="fightcoder-web"
+
+cfg=./cfg
+
+function build() {
+	local go="/usr/local/go"
+	if [ -d "$go" ]; then
+		export GOROOT="$go"
+		export PATH="$PATH:$GOROOT/bin"
+		export GOPATH="$GOROOT:$(pwd):$(pwd)/deps"
+	else
+		echo "Go文件不存在!"
+		exit 1
+	fi
+	echo "`go version`"
+	go build
+}
+
+function make_output {
+	local output="./output"
+	rm -rf $output &>/dev/null
+	mkdir -p $output &>/dev/null
+	(
+		cp -vrf $app $output && # 拷贝 二进制文件 至output目录
+		cp -vrf $cfg $output && # 拷贝 cfg配置文件目录 至output目录
+		echo -e "make output ok"
+	) || { echo -e "make output error"; rm -rf "./output"; exit 2; }
+}
+
+build
+
+make_output
+
+echo "build down"
+exit 0
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"time"
+
+	"github.com/appleboy/gin-revision-middleware"
+	"github.com/aviddiviner/gin-limit"
+	"github.com/gin-gonic/contrib/cors"
+	"github.com/gin-gonic/contrib/ginrus"
+	"github.com/gin-gonic/gin"
+	_ "github.com/open-fightcoder/oj-web/common"
+	"github.com/sirupsen/logrus"
+)
+
+func rootHandler(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"Hello": "world",
+	})
+	logrus.SetFormatter()
+	c.JSON(200, gin.H{"token": "aaa"})
+}
+
+func main() {
+
+	router := gin.New()
+	gin.Logger()
+	router.Use(cors.Default())
+	//修订中间件：显示版本信息在RespHeader
+	router.Use(revision.Middleware())
+	//限制最大请求并发量
+	router.Use(limit.MaxAllowed(20))
+	router.Use(ginrus.Ginrus(logrus.StandardLogger(), time.RFC3339, true))
+
+	router.GET("/", rootHandler)
+	router.Run(":" + "8000")
+}
