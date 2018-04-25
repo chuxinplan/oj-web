082917e2bfa7ac5d6dab5d7006f5678a08a4eb57
leeezm <leeezm@163.com>
2018-04-25 11:22:48 +0800

    add

--- common/store/redis.go
+++ common/store/redis.go
@@ -1,7 +1,26 @@
 package store
 
-func InitRedis() {
+import (
+	"fmt"
+	"sync"
 
+	"github.com/go-redis/redis"
+)
+
+var client *redis.Client
+var once sync.Once
+
+func InitRedis() *redis.Client {
+	once.Do(func() {
+		client := redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		})
+		pong, err := client.Ping().Result()
+		fmt.Println(pong, err)
+	})
+	return client
 }
 
 func CloseRedis() {
