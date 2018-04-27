e3a7d437466c1d9e555b654827b7d265fb57fecb
leeezm <leeezm@163.com>
2018-04-27 17:01:15 +0800

    add minio

--- common/store/minio_test.go
+++ common/store/minio_test.go
@@ -6,9 +6,11 @@ import (
 	"strings"
 
 	"github.com/minio/minio-go"
+	"github.com/open-fightcoder/oj-web/common/g"
 )
 
 func TestMinio(t *testing.T) {
+	g.LoadConfig("../../cfg/cfg.toml.debug")
 	InitMinio()
 	_, err := MinioClient.PutObject("ttt", "test.txt", strings.NewReader("aaaaaassssssdddd"), -1, minio.PutObjectOptions{ContentType: "application/octet-stream"})
 	if err != nil {
