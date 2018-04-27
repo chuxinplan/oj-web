cd739c41ee158def8019bb87482a0ccb0c56ad4a
SequinYF <catherine199787@outlook.com>
2018-04-27 15:57:28 +0800

    分文件

--- models/Member.go
+++ models/Member.go
@@ -1,1 +1,70 @@
 package models
+
+import (
+	. "github.com/open-fightcoder/oj-web/common/store"
+)
+
+//成员属性
+type GroupMember struct {
+	Id int64 	//id
+	Uid int64	//用户id
+	Gid int64	//组id
+}
+
+
+func MemberAdd(groupmember *GroupMember) (int64, error) {
+	return OrmWeb.Insert(groupmember)
+}
+
+func MemberRemove(id int64)error {
+	_, err := OrmWeb.Id(id).Delete(&GroupMember{})
+	return err
+}
+
+func MemberGetById(id int64) (*GroupMember, error) {
+	member := new(GroupMember)
+
+	has, err := OrmWeb.Id(id).Get(member)
+	if err != nil {
+		return nil, err
+	}
+	if !has {
+		return nil, nil
+	}
+	return member, nil
+
+}
+
+func MembersQueryByUid(uid int64) (*[]GroupMember, error) {
+	var groupmember []GroupMember
+
+	//不知道可不可以返回一个序列
+	has, err := OrmWeb.Where("uid=?", uid).Get(groupmember)
+	if err != nil {
+		return nil, err
+	}
+	if !has {
+		return nil, nil
+	}
+
+	return &groupmember, err
+}
+
+
+
+func MembersQueryByGid(gid int64)(*[]GroupMember, error) {
+
+	var groupmember []GroupMember
+
+	//不知道可不可以返回一个序列
+	has, err := OrmWeb.Where("gid=?", gid).Get(groupmember)
+	if err != nil {
+		return nil, err
+	}
+	if !has {
+		return nil, nil
+	}
+
+	return &groupmember, err
+}
+
--- models/group.go
+++ models/group.go
@@ -5,40 +5,23 @@ import (
 	. "github.com/open-fightcoder/oj-web/common/store"
 )
 
-type Group struct {
-	Info *GroupInfo
-	Memeber *[]GroupMember
-}
 
 //组属性
 type GroupInfo struct {
 	Id int64		//id
+	Uid int64		//组长id
 	Name string		//名称
 	Description string		//描述
 	Avator string	//头像
 }
 
-//成员属性
-type GroupMember struct {
-	Id int64	//用户id
-	Gid int64	//组id
-}
 
 func GroupCreate(groupinfo *GroupInfo)(int64, error){
 	return OrmWeb.Insert(groupinfo)
 }
 
-func GroupAdd(groupmember *GroupMember) (int64, error) {
-	return OrmWeb.Insert(groupmember)
-}
-
-func GroupDestroy(id int64) error {
+func GroupRemove(id int64) error {
 	_, err := OrmWeb.Id(id).Delete(&GroupInfo{})
-	return err
-}
-
-func GroupRemove(id int64)error {
-	_, err := OrmWeb.Id(id).Delete(&GroupMember{})
 	return err
 }
 
@@ -47,15 +30,8 @@ func GroupUpdate(groupinfo *GroupInfo) error {
 	return err
 }
 
-func MemberUpdate(member *GroupMember) error {
-	_, err:= OrmWeb.AllCols().ID(member.Id).Update(member)
-	return err
-}
-
-func GroupGetbyId(id int64)(*Group, error) {
-	group := new(Group)
+func GroupGetbyId(id int64)(*GroupInfo, error) {
 	groupinfo := new(GroupInfo)
-	groupmember := new([]GroupMember)
 
 	has, err := OrmWeb.Id(id).Get(groupinfo)
 	if err != nil {
@@ -64,23 +40,12 @@ func GroupGetbyId(id int64)(*Group, error) {
 	if !has {
 		return nil, nil
 	}
-
-	has, err = OrmWeb.Where("gid=?", groupinfo.Id).Get(groupmember)
-	if err != nil {
-		return nil, err
-	}
-
-	group.Memeber = groupmember
-	group.Info = groupinfo
-
-	return group, nil
+	return groupinfo, nil
 }
 
-func GroupGetbyName(name string) (*Group, error)  {
+func GroupGetbyName(name string) (*GroupInfo, error)  {
 
-	group := new(Group)
 	groupinfo := new(GroupInfo)
-	groupmember := new([]GroupMember)
 
 	has, err := OrmWeb.Where("name=?", name).Get(groupinfo)
 	if err != nil {
@@ -90,14 +55,5 @@ func GroupGetbyName(name string) (*Group, error)  {
 		return nil, nil
 	}
 
-	has, err = OrmWeb.Where("gid=?", groupinfo.Id).Get(groupmember)
-
-	if err != nil {
-		return nil, err
-	}
-
-	group.Memeber = groupmember
-	group.Info = groupinfo
-
-	return group, nil
+	return groupinfo, nil
 }
