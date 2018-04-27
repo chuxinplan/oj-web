4e96dafc780de725332f501b1db43f99bd9cee05
leeezm <leeezm@163.com>
2018-04-27 17:07:18 +0800

    complete

--- managers/problem/code.go
+++ managers/problem/code.go
@@ -3,6 +3,7 @@ package problem
 import (
 	"errors"
 
+	"github.com/open-fightcoder/oj-web/managers"
 	"github.com/open-fightcoder/oj-web/models"
 )
 
@@ -12,7 +13,7 @@ func CodeGet(userId int64, problemId int64) (map[string]interface{}, error) {
 		return nil, errors.New("Get code error!")
 	}
 	codeMess := map[string]interface{}{
-		"code":     userCode.SaveCode,
+		"code":     managers.GetCode(userCode.SaveCode),
 		"language": userCode.Language,
 	}
 	return codeMess, nil
@@ -24,11 +25,15 @@ func CodeSet(problemId int64, userId int64, saveCode string, language string) er
 		return err
 	}
 	var errorRet error
+	codePath, err := managers.SaveCode(saveCode)
+	if err != nil {
+		return err
+	}
 	if code == nil {
-		userCode := &models.UserCode{ProblemId: problemId, UserId: userId, SaveCode: saveCode, Language: language}
+		userCode := &models.UserCode{ProblemId: problemId, UserId: userId, SaveCode: codePath, Language: language}
 		_, errorRet = models.UserCodeCreate(userCode)
 	} else {
-		code.SaveCode = saveCode
+		code.SaveCode = codePath
 		code.Language = language
 		errorRet = models.UserCodeUpdate(code)
 	}
