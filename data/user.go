65298c9271689792c7d489751cbec4039b9818a4
leeezm <leeezm@163.com>
2018-04-28 13:30:19 +0800

    complete

--- data/user.go
+++ data/user.go
@@ -7,40 +7,43 @@ import (
 	"github.com/open-fightcoder/oj-web/models"
 )
 
-func UserRegister(userName string, email string, password string) error {
+func UserRegister(userName string, email string, password string) (int64, error) {
 	session := OrmWeb.NewSession()
 	defer session.Close()
 
 	err := session.Begin()
+	if err != nil {
+		return 0, errors.New("注册失败")
+	}
 	has, err := session.Where("email = ? for update", email).Get(&models.Account{})
 	if err != nil {
-		return errors.New("注册失败")
+		return 0, errors.New("注册失败")
 	}
 	if has {
-		return errors.New("邮箱已存在")
+		return 0, errors.New("邮箱已存在")
 	}
 	has, err = session.Where("user_name = ? for update", userName).Get(&models.User{})
 	if err != nil {
-		return errors.New("注册失败")
+		return 0, errors.New("注册失败")
 	}
 	if has {
-		return errors.New("用户名已存在")
+		return 0, errors.New("用户名已存在")
 	}
 	account := &models.Account{Email: email, Password: password}
 	_, err = session.Insert(account)
 	if err != nil {
 		session.Rollback()
-		return errors.New("注册失败")
+		return 0, errors.New("注册失败")
 	}
 	user := &models.User{AccountId: account.Id, UserName: userName}
 	_, err = session.Insert(user)
 	if err != nil {
 		session.Rollback()
-		return errors.New("注册失败")
+		return 0, errors.New("注册失败")
 	}
 	err = session.Commit()
 	if err != nil {
-		return errors.New("注册失败")
+		return 0, errors.New("注册失败")
 	}
-	return nil
+	return user.Id, nil
 }
