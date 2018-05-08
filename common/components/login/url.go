ca44d1186f8f3cfdec9f8226d2e248f7e1a0af4d
leeezm <leeezm@163.com>
2018-05-08 22:55:24 +0800

    upS

--- common/components/login/url.go
+++ common/components/login/url.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"strings"
+
 	"github.com/pkg/errors"
 )
 
@@ -60,12 +62,36 @@ func (this *Url) get(url string, keysArr map[string]string) (string, error) {
 }
 
 /**
+ * post_contents
+ * 服务器通过post请求获得内容
+ * @param string url       请求的url,拼接后的
+ * @return string          请求返回的内容
+ */
+func (this *Url) post_contents(url string, params string) (string, error) {
+	resp, err := http.Post(url, "application/x-www-form-urlencoded",
+		strings.NewReader(params))
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode == 200 {
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+		return string(body), nil
+	}
+	return "", errors.New("请求错误")
+}
+
+/**
  * post
  * post方式请求资源
  * @param string $url       基于的baseUrl
  * @param array $keysArr    请求的参数列表
  * @return string           返回的资源内容
  */
-func (this *Url) post(url string, keysArr []map[string]string) string {
-	return ""
+func (this *Url) post(url string, keysArr map[string]string) (string, error) {
+	requestUrl := this.combineURL("", keysArr)
+	return this.post_contents(url, requestUrl)
 }
