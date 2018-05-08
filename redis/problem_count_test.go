a8683cda7a2ade7463e20a28d5267684f036216f
leeezm <leeezm@163.com>
2018-05-08 11:45:39 +0800

    upS

--- redis/problem_count_test.go
+++ redis/problem_count_test.go
@@ -13,7 +13,7 @@ func TestProblemCountSet(t *testing.T) {
 	g.LoadConfig("../cfg/cfg.toml.debug")
 	store.InitRedis()
 
-	aa := ProblemCountSet(13, "{\"ac_num\":12,\"total_num\":42}")
+	aa := ProblemCountSet(14, "accepted\":1,\"fail_num\":3,\"wrong_answer\":1,\"compilation_error\":1,\"time_limit_exceeded\":5,\"memory_limit_exceeded\":1,\"output_limit_exceeded\":1,\"runtime_error\":3,\"system_error\":2}")
 	fmt.Println(aa)
 }
 
--- redis/submit_count.go
+++ redis/submit_count.go
@@ -18,3 +18,12 @@ func SubmitCountGet(userId int64) (string, error) {
 	}
 	return fmt.Sprint(res.Val()[0]), nil
 }
+
+func SubmitCountSet(userId int64, mess string) bool {
+	res := RedisClient.HMSet("submit_count", map[string]interface{}{strconv.FormatInt(userId, 10): mess})
+	str, _ := res.Result()
+	if str == "OK" {
+		return true
+	}
+	return false
+}
